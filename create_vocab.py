2f1f89570616a5d1e99369d8880d4363f8c51307
James Bower <bower.james1996@gmail.com>
2024-10-17 23:15:47 +0100

    moved to hugging face trainer

--- create_vocab.py
+++ create_vocab.py
@@ -1,17 +1,13 @@
 rows = ["1", "2", "3", "4", "5", "6", "7", "8"]
 columns = ["a", "b", "c", "d", "e", "f", "g", "h"]
+squares = [column + row for column in columns for row in rows]
 pieces = ["r", "n", "b", "q", "k"]
 
 with open("vocab.txt", "w") as f:
-    for row in rows:
-        for column in columns:
-            for row_2 in rows:
-                for column_2 in columns:
-                    if not (row == row_2 and column == column_2):
-                        f.write(f"{column}{row}{column_2}{row_2}\n")
-                        if row_2 in ["1", "8"] and row != row_2:
-                            for piece in pieces:
-                                f.write(f"{column}{row}{column_2}{row_2}{piece}\n")
+    for square in squares:
+        f.write(f"{square}\n")
+    for piece in pieces:
+        f.write(f"{piece}\n")
 
                         
 
--- src/tokenizer/tokenizer.py
+++ src/tokenizer/tokenizer.py
@@ -1,7 +1,6 @@
-from transformers import PreTrainedTokenizer
-import collections
 import torch
 import os
+
 
 file = __file__
 
@@ -14,10 +13,10 @@ class ChessTokenizer:
         self.max_length = kwargs.get("max_length", 256)
 
         # Define special tokens
-        self.pad_token = "[PAD]"
         self.eos_token = "[EOS]"
+        self.eom_token = "[EOM]"
 
-        self.vocab = [self.pad_token, self.eos_token] + vocab
+        self.vocab = [self.eos_token, self.eom_token] + vocab
         self.vocab_size = len(self.vocab)
 
         self.ids_to_tokens = {idx: token for idx, token in enumerate(self.vocab)}
@@ -27,8 +26,8 @@ class ChessTokenizer:
         
         # Assign IDs to special tokens
         self.pad_token_id = 0
-        self.eos_token_id = 1
-        
+        self.eos_token_id = 0
+        self.eom_token_id = 1
     
     def load_vocab(self, vocab_file):
         with open(vocab_file, 'r', encoding='utf-8') as f:
@@ -37,34 +36,49 @@ class ChessTokenizer:
     
     def encode(self, text: str | list[str]):
         if isinstance(text, str):
+            text = text.strip()
 
             text_list = self._tokenize(text)
-            text_list = text_list
 
             ids = self._convert_tokens_to_ids(text_list)
 
             return ids
 
         elif isinstance(text, list):
-            text_lists = [self._tokenize(t) for t in text]
-            text_lists = [
-                text_list + [self.eos_token] for text_list in text_lists
-            ]
+            text_lists = [self._tokenize(t.strip()) for t in text]
 
             idss = [self._convert_tokens_to_ids(text_list) for text_list in text_lists]
 
             return idss
 
-    def _tokenize(self, text: str):
-        return text.split(" ")
+    def _tokenize(self, text: str) -> list[str]:
+        tokens = []
+        moves = text.split(" ")
+        for move in moves:
+            assert len(move) in [4, 5]
+            square_1 = move[:2]
+            square_2 = move[2:4]
+            tokens.append(square_1)
+            tokens.append(square_2)
+            if len(move) ==5:
+                piece = move[4]
+                tokens.append(piece)
+            tokens.append("[EOM]")
+        tokens.append("[EOS]")
+        return tokens
+            
+        
     
     def _convert_tokens_to_ids(self, tokens: list[str]):
         return [self.tokens_to_ids[token] for token in tokens]
     
-    def __call__(self, text: str | list[str], padding: bool = True, max_length: int = None, return_tensors: bool = False):
+    def __call__(self, text: str | list[str], padding: bool = True, max_length: int | None = None, return_tensors: str | None = None):
         if isinstance(text, str):
             text = [text]
-
+            batched=False
+        else:
+            batched=True
+        
         if return_tensors:
             padding = True
 
@@ -73,9 +87,6 @@ class ChessTokenizer:
         attention_mask = [
             [1] * len(ids_) for ids_ in ids
         ]
-
-        ids = [ids_ + [self.eos_token_id] for ids_ in ids]
-        attention_mask = [attention_mask_ + [0] for attention_mask_ in attention_mask]
 
         if padding:
             maximum_length = max([len(ids_) for ids_ in ids])
@@ -92,11 +103,51 @@ class ChessTokenizer:
                 attention_mask_ + [0] * (max_length - len(attention_mask_)) if len(attention_mask_) < max_length else attention_mask_[:max_length] for attention_mask_ in attention_mask
             ]
 
-        if return_tensors:
+        if not batched:
+            ids = ids[0]
+            attention_mask = attention_mask[0]
+
+        if return_tensors=="pt":
             ids = torch.tensor(ids)
             attention_mask = torch.tensor(attention_mask)
 
         return {"input_ids": ids, "attention_mask": attention_mask}
+            
+    def pad(self,encoded_inputs, return_tensors: str | None = None, **kwargs):
+        input_ids = [
+            encoded_input["input_ids"] for encoded_input in encoded_inputs
+        ]
+        attention_mask = [
+            encoded_input["attention_mask"] for encoded_input in encoded_inputs
+        ]
+
+        maximum_length = max(
+            [len(input_ids_) for input_ids_ in input_ids]
+        )
+
+        max_length = min(maximum_length, self.max_length)
+
+
+
+        maximum_length = max([len(input_ids_) for input_ids_ in input_ids])
+        if maximum_length > self.max_length:
+            max_length = self.max_length
+        else:
+            max_length = maximum_length
+
+        input_ids = [
+            input_ids_ + [self.pad_token_id] * (max_length - len(input_ids_)) if len(input_ids_) < max_length else input_ids_[:max_length] for input_ids_ in input_ids
+        ]
+
+        attention_mask = [
+            attention_mask_ + [0] * (max_length - len(attention_mask_)) if len(attention_mask_) < max_length else attention_mask_[:max_length] for attention_mask_ in attention_mask
+        ]
+
+        if return_tensors=="pt":
+            input_ids = torch.tensor(input_ids)
+            attention_mask = torch.tensor(attention_mask)
+
+        return {"input_ids": input_ids, "attention_mask": attention_mask}
             
             
         
